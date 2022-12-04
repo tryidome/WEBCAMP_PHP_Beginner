3f1911e0e56a6c769a334a7cb5028ff71cab8db1
tryidome <113878350+tryidome@users.noreply.github.com>
2022-12-04 07:21:46 +0000

    bit_operation.phpを書いた

---
+++ bit_operation.php
@@ -0,0 +1,20 @@
+<?php
+
+$i = random_int(0,255);
+$j = random_int(0,255);
+
+// AND
+$k = $i & $j;
+printf("%x & %x = %x \n", $i, $j, $k);
+
+// OR
+$k = $i | $j;
+printf("%x | %x = %x \n", $i, $j, $k);
+
+// XOR
+$k = $i ^ $j;
+printf("%x ^ %x = %x \n", $i, $j, $k);
+
+// NOT
+$k = ~$i;
+printf("~%x = %x \n", $i, $k);
