c252badc32abeb1caf62198066715653ba516ccc
tryidome <113878350+tryidome@users.noreply.github.com>
2022-12-04 07:07:57 +0000

    string_add.phpを書いた

---
+++ string_add.php
@@ -0,0 +1,10 @@
+<?php
+
+$string1 = "Hello";
+$string2 = "World.";
+
+$string_dot = $string1 . $string2 . "\n";
+echo $string_dot;
+
+$string_dquote = "{$string1}{$string2} \n";
+echo $string_dquote;
