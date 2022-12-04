67c94908554307d6e9f217fc31e75a918b42f7ce
tryidome <113878350+tryidome@users.noreply.github.com>
2022-12-04 08:04:07 +0000

    define.phpを書いた

--- bit_operation.php
+++ bit_operation.php
@@ -1,20 +1,8 @@
 <?php
 
-$i = random_int(0,255);
-$j = random_int(0,255);
+$i = 1800;
+$j = 1.1
 
-// AND
-$k = $i & $j;
-printf("%x & %x = %x \n", $i, $j, $k);
+$k = $i * $j;
 
-// OR
-$k = $i | $j;
-printf("%x | %x = %x \n", $i, $j, $k);
-
-// XOR
-$k = $i ^ $j;
-printf("%x ^ %x = %x \n", $i, $j, $k);
-
-// NOT
-$k = ~$i;
-printf("~%x = %x \n", $i, $k);
+echo $k;
